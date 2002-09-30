3bb58f7acfd0637ac23274ac559479bf768ee041
Jim Weaver <jimweaver@users.sourceforge.net>
2002-09-30 18:20:14 +0000

    Addeed named button submit, updated WebFixtureTest

--- jWebUnit/fitplugin/test/net/sourceforge/jwebunit/fit/WebFixtureTest.java
+++ jWebUnit/fitplugin/test/net/sourceforge/jwebunit/fit/WebFixtureTest.java
@@ -11,10 +11,11 @@ import com.meterware.pseudoserver.PseudoServer;
 
 import java.text.MessageFormat;
 import java.text.ParseException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 
 public class WebFixtureTest extends TestCase {
+    PrintWriter output;
+
     static String baseUrlFmt = "http://localhost:";
 
     static String startTableFmt =
@@ -22,8 +23,8 @@ public class WebFixtureTest extends TestCase {
             "<tr><td>jwebunit.fit.WebFixture</td></tr>" +
             "<tr><td>baseUrl</td><td>{0}</td></tr>" +
             "<tr><td>begin</td><td>start</td></tr>" +
-            "<tr><td>check</td><td>assertTitleEquals</td><td>Start Page</td></tr>" +
-            "<tr><td>check</td><td>assertTextInTable</td><td>t1</td><td>table text</td></tr>" +
+            "<tr><td>check</td><td>title equals</td><td>Start Page</td></tr>" +
+            "<tr><td>check</td><td>text in table</td><td>t1</td><td>table text</td></tr>" +
             "</table>";
 
     static String enterTableFmt =
@@ -43,6 +44,15 @@ public class WebFixtureTest extends TestCase {
             "<tr><td>press</td><td>submit</td></tr>" +
             "</table>";
 
+    static String pressNamedTableFmt =
+            "<table border=\"1\">" +
+            "<tr><td>jwebunit.fit.WebFixture</td></tr>" +
+            "<tr><td>baseUrl</td><td>{0}</td></tr>" +
+            "<tr><td>begin</td><td>multiForm</td></tr>" +
+            "<tr><td>enter</td><td>field1</td><td>testValue</td></tr>" +
+            "<tr><td>press</td><td>submit</td><td>Button1</td></tr>" +
+            "</table>";
+
     static String pressLinkFmt =
             "<table border=\"1\">" +
             "<tr><td>jwebunit.fit.WebFixture</td></tr>" +
@@ -56,9 +66,9 @@ public class WebFixtureTest extends TestCase {
     private WebFixture fixture;
     private PseudoServer server;
     private Parse tables;
-    private String expectedTable = "<table border=\"1\"><tr><td>jwebunit.fit.WebFixture</td></tr><tr><td>baseUrl</td><td>http://localhost:2600/</td></tr><tr><td>begin</td><td>start</td></tr><tr><td>check</td><td>assertTitleEquals</td><td bgcolor=\"#cfffcf\">Start Page</td></tr><tr><td>check</td><td>assertTextInTable</td><td bgcolor=\"#cfffcf\">t1</td><td>table text</td></tr></table>";
+    private String expectedTable = "<table border=\"1\"><tr><td>jwebunit.fit.WebFixture</td></tr><tr><td>baseUrl</td><td>{0}</td></tr><tr><td>begin</td><td>start</td></tr><tr><td>check</td><td>title equals</td><td bgcolor=\"#cfffcf\">Start Page</td></tr><tr><td>check</td><td>text in table</td><td>t1</td><td bgcolor=\"#cfffcf\">table text</td></tr></table>";
 
-    public WebFixtureTest(String s) {
+    public WebFixtureTest(String s) throws IOException {
         super(s);
     }
 
@@ -66,7 +76,14 @@ public class WebFixtureTest extends TestCase {
         super.setUp();
         fixture = new WebFixture();
         buildSite();
+        output = new PrintWriter(new BufferedWriter(new FileWriter("testoutput.html")));
         baseUrl = baseUrlFmt + server.getConnectedPort() + "/";
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+        tables.print(output);
+        output.close();
     }
 
     private void parseTable(String inputTable) throws ParseException {
@@ -87,10 +104,22 @@ public class WebFixtureTest extends TestCase {
                 "<table summary=\"t1\">" +
                 "<tr><td>table text</td></tr>" +
                 "</table></body></html>");
+        server.setResource("/multiForm",
+                "<html><head><title>Multi-Button Page</title></head><body>" +
+                "<form method=GET action=\"/mainpage\" >" +
+                "<input type=\"text\" name=\"field1\"/>" +
+                "<input type=\"submit\"name=\"Button1\" value=\"Button_1\"/>" +
+                "<input type=\"submit\"name=\"Button2\" value=\"Button_2\"/>" +
+                "</form></body></html>");
         server.setResource("/mainpage?field1=testValue",
                 "<html><head><title>Main Page</title></head>" +
                 "<body>" +
                 "<h1>Main Page</h1>" +
+                "</body></html>");
+        server.setResource("/mainpage?field1=testValue&Button1=Button_1",
+                "<html><head><title>Button 1 Pressed Page</title></head>" +
+                "<body>" +
+                "<h1>Button 1 Pressed</h1>" +
                 "</body></html>");
         server.setResource("/mainpage", "<html><head><title>Wrong Page</title></head><body></body></html>");
     }
@@ -99,7 +128,6 @@ public class WebFixtureTest extends TestCase {
         parseTable(formatTable(startTableFmt));
         assertEquals(baseUrl, fixture.getBaseUrl());
         fixture.tester.assertTitleEquals("Start Page");
-        //System.out.println(tableToString());
         assertEquals(formatTable(expectedTable), tableToString());
     }
 
@@ -118,6 +146,12 @@ public class WebFixtureTest extends TestCase {
     public void testPressSubmit() throws ParseException {
         parseTable(formatTable(pressTableFmt));
         fixture.tester.assertTitleEquals("Main Page");
+    }
+
+    public void testPressSubmitWithName() throws ParseException {
+        parseTable(formatTable(pressNamedTableFmt));
+        fixture.tester.dumpResponse(System.out);
+        fixture.tester.assertTitleEquals("Button 1 Pressed Page");
     }
 
     public void testPressLink() throws ParseException {
