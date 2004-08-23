648a039f53d7bc6e704f5c6e53939978a6b4d8dd
Martijn Dashorst <martijn.dashorst@gmail.com>
2004-08-23 22:13:44 +0000

    Moved NavigationTest from TestServer to Jetty.

--- jWebUnit/test/net/sourceforge/jwebunit/NavigationTest.java
+++ jWebUnit/test/net/sourceforge/jwebunit/NavigationTest.java
@@ -1,111 +1,111 @@
 package net.sourceforge.jwebunit;
 
+import net.sourceforge.jwebunit.util.JettySetup;
 import junit.framework.AssertionFailedError;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 /**
- * Test url navigation methods on WebTestCase (starting at a url, navigating links).
- *
+ * Test url navigation methods on WebTestCase (starting at a url, navigating
+ * links).
+ * 
  * @author Jim Weaver
  * @author Wilkes Joiner
  */
-public class NavigationTest extends JWebUnitTest {
+public class NavigationTest extends WebTestCase {
+
+    private String hostPath;
+
+    public static Test suite() {
+        Test suite = new TestSuite(NavigationTest.class);
+        return new JettySetup(suite);
+    }
 
     public void setUp() throws Exception {
-        super.setUp();
+        hostPath = "http://localhost:8081/jwebunit";
+        getTestContext().setBaseUrl(hostPath + "/NavigationTest");
     }
 
     public void testBeginAt() {
-        defineResource("/blah.html", "");
         beginAt("/blah.html");
     }
 
     public void testForwardSlashConfusion() throws Exception {
-        defineWebPage("/app/blah", "here");
-        getTestContext().setBaseUrl(hostPath + "/app");
         beginAt("/blah.html");
         beginAt("blah.html");
-        getTestContext().setBaseUrl(hostPath + "/app/");
+        getTestContext().setBaseUrl(hostPath + "/NavigationTest/");
         beginAt("/blah.html");
         beginAt("blah.html");
     }
 
     public void testInvalidBeginAt() {
-        defineResource("/", "");
         try {
             beginAt("/nosuchresource.html");
+            fail("Expected exception");
         } catch (Throwable t) {
-            return;
         }
-        fail("Expected exception");
     }
 
     public void testClickLinkWithText() {
-        gotoLinkTestPage();
+        beginAt("/pageWithLink.html");
+        assertTitleEquals("pageWithLink");
+
         clickLinkWithText("an active link");
         assertTitleEquals("targetPage");
     }
 
     public void testClickLinkWithTextN() {
-        gotoLinkTestPage();
+        beginAt("/pageWithLink.html");
+        assertTitleEquals("pageWithLink");
+
         clickLinkWithText("an active link", 0);
         assertTitleEquals("targetPage");
+
         beginAt("/pageWithLink.html");
         clickLinkWithText("an active link", 1);
+
         assertTitleEquals("targetPage2");
         beginAt("/pageWithLink.html");
         try {
             clickLinkWithText("an active link", 2);
             fail();
         } catch (AssertionFailedError expected) {
-            assertEquals(
-                "Link with text [an active link] and index 2 "
-                    + "not found in response.",
-                expected.getMessage());
+            assertEquals("Link with text [an active link] and index 2 "
+                    + "not found in response.", expected.getMessage());
         }
         assertTitleEquals("pageWithLink");
     }
 
     public void testClickLinkWithTextAfterText() {
-        defineWebPage("pageWithLink",
-            "First: <a href=\"/targetPage1.html\">link text</a>\n" +
-            "Second: <a href=\"/targetPage2.html\">link text</a>)\n");
-        defineWebPage("targetPage1", "");
-        defineWebPage("targetPage2", "");
-
-        beginAt("/pageWithLink.html");
+        beginAt("/pageWithLinkWithTextAfterText.html");
         clickLinkWithTextAfterText("link text", "First:");
-        assertTitleEquals("targetPage1");
+        assertTitleEquals("targetPage");
 
-        beginAt("/pageWithLink.html");
+        beginAt("/pageWithLinkWithTextAfterText.html");
         clickLinkWithTextAfterText("link text", "Second:");
         assertTitleEquals("targetPage2");
     }
 
     public void testClickLinkWithImage() {
-        gotoLinkTestPage();
+        beginAt("/pageWithLink.html");
+        assertTitleEquals("pageWithLink");
+
         clickLinkWithImage("graphic.jpg");
         assertTitleEquals("targetPage2");
     }
 
     public void testClickLinkByID() {
-        gotoLinkTestPage();
+        beginAt("/pageWithLink.html");
+        assertTitleEquals("pageWithLink");
+
         clickLink("activeID");
         assertTitleEquals("targetPage");
     }
 
-    private void gotoLinkTestPage() {
-        defineWebPage("pageWithLink",
-            "<a href=\"/targetPage.html\" id=\"activeID\">an <b>active</b> link</A>\n" +
-            "<a href=\"/targetPage2.html\"><img src=\"graphic.jpg\"/></a>)\n" +
-            "<a href=\"/targetPage2.html\">an active <i>link</i></a>\n");
-        defineWebPage("targetPage", "");
-        defineWebPage("targetPage2", "");
+    public void testInvalidClickLink() {
         beginAt("/pageWithLink.html");
         assertTitleEquals("pageWithLink");
-    }
 
-    public void testInvalidClickLink() {
-        gotoLinkTestPage();
         try {
             clickLinkWithText("no such link");
         } catch (Throwable t) {
@@ -115,11 +115,9 @@ public class NavigationTest extends JWebUnitTest {
     }
 
     public void testGotoPage() {
-        defineWebPage("page1", "page1");
-        defineWebPage("page2", "page2");
-        beginAt("/page1.html");
-        assertTitleEquals("page1");
-        gotoPage("page2.html");
-        assertTitleEquals("page2");
+        beginAt("/targetPage.html");
+        assertTitleEquals("targetPage");
+        gotoPage("/targetPage2.html");
+        assertTitleEquals("targetPage2");
     }
 }
