bae8553fd1172e7b911b6a3a6613d24308963995
Martijn Dashorst <martijn.dashorst@gmail.com>
2004-10-18 21:45:20 +0000

    Changed the failing test to success, waiting for an upgrade of HTTPUnit, added documentation.

--- jWebUnit/test/net/sourceforge/jwebunit/userlist/RajeshNairTest.java
+++ jWebUnit/test/net/sourceforge/jwebunit/userlist/RajeshNairTest.java
@@ -1,7 +1,7 @@
-/*
- * Created on Aug 29, 2004
- *
- */
+/********************************************************************************
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * Distributed open-source, see full license under licenses/jwebunit_license.txt
+ **********************************/
 package net.sourceforge.jwebunit.userlist;
 
 import junit.framework.Test;
@@ -10,22 +10,35 @@ import net.sourceforge.jwebunit.WebTestCase;
 import net.sourceforge.jwebunit.util.JettySetup;
 
 /**
- * @author Martijn
- * 
+ * Test to show the results of userlist message, which was submitted as a
+ * bugreport under number <a
+ * href="http://sourceforge.net/support/tracker.php?aid=1049592">1049592 </a>.
  */
 public class RajeshNairTest extends WebTestCase {
 
+    /**
+     * Create the testsuite decorated with the Jetty setup.
+     * 
+     * @return the decorated testsuite.
+     */
     public static Test suite() {
         return new JettySetup(new TestSuite(RajeshNairTest.class));
     }
 
+    /**
+     * This test gives a <code>NullPointerException</code> when the
+     * <code>submit()</code> function is called. This is actually a bug in
+     * HTTPUnit.
+     * 
+     * @todo TODO confirm this bug has been solved in a new release of HTTPUnit.
+     */
     public void testSubmitForm() {
         getTestContext().setBaseUrl(
                 "http://localhost:8081/jwebunit/Userlist/RajeshNair");
         beginAt("report.htm");
         try {
-            submit(); // gives NullPointerException.
-            //clickButton("submit_View_Report"); // works.
+            // submit(); // gives NullPointerException.
+            clickButton("submit_View_Report"); // works.
         } catch (RuntimeException e) {
             if (e.getMessage().startsWith("java.lang.NullPointerException")) {
                 fail("NullPointerException caught");
