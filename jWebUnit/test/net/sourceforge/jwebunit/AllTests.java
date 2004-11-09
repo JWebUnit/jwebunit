53fb8f4c15a4a56280b1edd7f99111d711d4f9c2
Jim Weaver <jimweaver@users.sourceforge.net>
2004-11-09 19:37:01 +0000

    jw : uncommenting unit tests - brief cruisecontrol experiment

--- jWebUnit/test/net/sourceforge/jwebunit/AllTests.java
+++ jWebUnit/test/net/sourceforge/jwebunit/AllTests.java
@@ -27,25 +27,23 @@ public class AllTests extends TestSuite {
      */
     public static Test suite() {
         TestSuite suite = new TestSuite();
-//        suite.addTestSuite(NavigationTest.class);
-//        suite.addTestSuite(FormSubmissionTest.class);
+        suite.addTestSuite(NavigationTest.class);
+        suite.addTestSuite(FormSubmissionTest.class);
         suite.addTestSuite(WebAssertionsTest.class);
-//        suite.addTestSuite(TestContextTest.class);
-//        suite.addTestSuite(ExpectedTableTest.class);
-//        suite.addTestSuite(ExpectedTableAssertionsTest.class);
-//        suite.addTestSuite(FormAssertionsTest.class);
-//        suite.addTestSuite(TableAssertionsTest.class);
-//        suite.addTestSuite(TextAndElementWalkerTest.class);
-//        suite.addTestSuite(FramesAndWindowsTest.class);
-//        suite.addTestSuite(JavaScriptEventsTest.class);
-//        suite.addTestSuite(ServletUnitTest.class);
-//        suite.addTestSuite(MethodInvokerTest.class);
-//        suite.addTestSuite(WebCookieTest.class);
-//        suite.addTestSuite(FormAssertionBug.class);
-//        suite.addTestSuite(HelloWorldTest.class);
-//        return new JettySetup(suite);	
-        // test change for cc
-          return suite;
+        suite.addTestSuite(TestContextTest.class);
+        suite.addTestSuite(ExpectedTableTest.class);
+        suite.addTestSuite(ExpectedTableAssertionsTest.class);
+        suite.addTestSuite(FormAssertionsTest.class);
+        suite.addTestSuite(TableAssertionsTest.class);
+        suite.addTestSuite(TextAndElementWalkerTest.class);
+        suite.addTestSuite(FramesAndWindowsTest.class);
+        suite.addTestSuite(JavaScriptEventsTest.class);
+        suite.addTestSuite(ServletUnitTest.class);
+        suite.addTestSuite(MethodInvokerTest.class);
+        suite.addTestSuite(WebCookieTest.class);
+        suite.addTestSuite(FormAssertionBug.class);
+        suite.addTestSuite(HelloWorldTest.class);
+        return new JettySetup(suite);	
     }
 
     public static void main(String[] args) {
