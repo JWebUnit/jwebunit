616e974304bcde7dab41b64210d88bea73937c6e
Staffan Olsson <solsson@gmail.com>
2006-01-03 13:47:22 +0000

    Implemented use of jetty for fixture test. Use of the PseudoWebApp has been commented out. Temporarily added dummy files for the dynamic pages needed.

--- jWebUnit/fitplugin/test/net/sourceforge/jwebunit/fit/WebFixtureTest.java
+++ jWebUnit/fitplugin/test/net/sourceforge/jwebunit/fit/WebFixtureTest.java
@@ -5,19 +5,85 @@
  */
 package net.sourceforge.jwebunit.fit;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sourceforge.jwebunit.WebTester;
+
+import org.mortbay.http.HttpContext;
+import org.mortbay.http.HttpServer;
+import org.mortbay.http.SocketListener;
+import org.mortbay.http.handler.ResourceHandler;
+import org.mortbay.util.Resource;
+
 import junit.framework.TestCase;
 
 public class WebFixtureTest extends TestCase {
 
     public static final int MINIMUM_TESTS = 50;
     public static final String TEST_ROOT = "fitplugin/test/";
+    public static final String TEST_HTML_FOLDER = "sampleHtml/";
+    public static final int JETTY_PORT_DEFAULT = 8081;
+    public static final String JETTY_PORT_PROPERTY = "jetty.port";
+    private static final String HOST = "localhost";
+    
+    private HttpServer server = null;
+    
+    // translation between urls used in .fit files and the urls in jetty context
+    private static Map oldUrls = null;
+    static {
+        oldUrls = new HashMap();
+        oldUrls.put("/colorForm", "/ColorForm.html");
+        oldUrls.put("/moriaDoorForm", "/MoriaDoorForm.html");
+        oldUrls.put("/personalInfoForm", "/PersonalInfoForm.html");
+        oldUrls.put("/sampleMenu", "/SampleMenu.html");
+        oldUrls.put("/pageWithPopupLink", "/pageWithPopupLink.html");
+        oldUrls.put("/menu", "/menu.html");
+    }
     
     public WebFixtureTest(String s) {
         super(s);
     }
 
+    protected void setUp() throws Exception {
+        super.setUp();
+        server = new HttpServer();
+        SocketListener listener = new SocketListener();
+        listener.setPort(getJettyPort());
+        listener.setHost(HOST);
+        listener.setMinThreads(1);
+        listener.setMaxThreads(10);
+        server.addListener(listener);
+        // add the tiles in sampleHtml to context
+        final HttpContext context = server.addContext("/");
+        context.setResourceBase(TEST_ROOT + TEST_HTML_FOLDER);
+        assertTrue("Should find index.html in the configured jetty context",
+                context.getResource("index.html").exists());
+        context.addHandler(new ResourceHandler() {
+            protected Resource getResource(String pathInContext) throws IOException {
+                Resource r = super.getResource(pathInContext);
+                if (!r.exists() && oldUrls.containsKey(pathInContext)) { // don't want to update the .fit files until the old tests work
+                    r = super.getResource(oldUrls.get(pathInContext).toString());
+                }
+                assertTrue("The requested resource '" + pathInContext + "' must exist", r.exists());
+                return r;
+            }
+        });
+        // run
+        server.start();
+    }
+
+    public void testFixtureStart() throws Exception {
+        WebFixture fixture = new WebFixture();
+        fixture.setBaseUrl("http://" + HOST + ":" + getJettyPort());
+        WebTester tester = WebFixture.tester;
+        tester.beginAt("index.html");
+        tester.assertTitleEquals("Test root");
+    }
+    
     public void testWebFixture() throws Exception {
-        new PseudoWebApp();
+        //new PseudoWebApp();
         // avoid the need of the system property, always use .fit files for input
         RunnerUtility.overrideSystemPropertyAndUseWikiParser = true;
         // run the tests
@@ -36,6 +102,17 @@ public class WebFixtureTest extends TestCase {
 			testRunner.getResultWriter().getCounts().wrong);
         assertEquals("Exceptions detected. Check " + resultsUrl + ".", 0, 
         	testRunner.getResultWriter().getCounts().exceptions);
+    }
+    
+    protected void tearDown() throws Exception {
+        server.stop();
+        server = null;
+        super.tearDown();
+    }
+    
+    public static int getJettyPort() {
+        String p = System.getProperty(JETTY_PORT_PROPERTY, "" + JETTY_PORT_DEFAULT);
+        return Integer.parseInt(p);
     }
 
 }
