a6a90d0f1523bb1d462aaac256bcabde559bcd6f
Julien HENRY <henryju@yahoo.fr>
2006-05-12 08:21:53 +0000

    Change javax.servlet dependency to use standard jar instead of Jetty one.

--- jwebunit-core/src/main/java/net/sourceforge/jwebunit/TestContext.java
+++ jwebunit-core/src/main/java/net/sourceforge/jwebunit/TestContext.java
@@ -8,7 +8,6 @@ import javax.servlet.http.Cookie;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
