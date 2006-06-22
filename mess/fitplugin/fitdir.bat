ab99d4fb5b2467b7b29b3ce6afd528b9415ae8f9
Julien HENRY <henryju@yahoo.fr>
2006-06-22 13:48:50 +0000

    Clean trunk (remove httpunit and jacobie plugins) and update version in pom to 2.0.

--- mess/fitplugin/fitdir.bat
+++
@@ -1,3 +0,0 @@
-set projectDir=C:\wp\jwebunit\jWebunit
-set cpath=%projectDir%\classes;%projectDir%\lib\fit.jar;%projectDir%\lib\httpunit.jar;%projectDir%\lib\js.jar;%projectDir%\lib\junit.jar;%projectDir%\lib\servlet.jar;%projectDir%\lib\nekohtml.jar;%projectDir%\lib\xercesImpl.jar;%projectDir%\lib\xml-apis.jar
-java -cp %cpath% -Dwiki=true net.sourceforge.jwebunit.fit.DirectoryRunner %1 %2
