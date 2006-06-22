1a724242840970a51c07f90d9132800bccc5519b
Julien HENRY <henryju@yahoo.fr>
2006-06-22 13:46:49 +0000

    Clean branch (remove selenium and jacobie plugins).

--- mess/fitplugin/fit.bat
+++
@@ -1,2 +0,0 @@
-set cpath=..\classes;..\lib\httpunit.jar;..\lib\js.jar;..\lib\junit.jar;..\lib\servlet.jar;..\lib\Tidy.jar;..\lib\xercesImpl.jar;..\lib\xml-apis.jar
-java -cp %cpath% fit.FileRunner %1 %2
