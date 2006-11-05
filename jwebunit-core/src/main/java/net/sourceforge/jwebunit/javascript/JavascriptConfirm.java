8cd68c5dec67d51277de065590264dd7adf9e28a
Julien HENRY <henryju@yahoo.fr>
2006-11-05 14:14:36 +0000

    Commit 3 missing files (oups) for Javascript boxes support.

---
+++ jwebunit-core/src/main/java/net/sourceforge/jwebunit/javascript/JavascriptConfirm.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * jWebUnit project (http://jwebunit.sourceforge.net)                         *
+ * Distributed open-source, see full license under LICENCE.txt                *
+ ******************************************************************************/
+package net.sourceforge.jwebunit.javascript;
+
+public class JavascriptConfirm {
+	
+	private String message;
+	
+	private boolean action;
+	
+	public JavascriptConfirm(String message, boolean action) {
+		this.message=message;
+		this.action=action;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public boolean getAction() {
+		return action;
+	}
+
+}
---
+++ jwebunit-core/src/main/java/net/sourceforge/jwebunit/javascript/JavascriptPrompt.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * jWebUnit project (http://jwebunit.sourceforge.net)                         *
+ * Distributed open-source, see full license under LICENCE.txt                *
+ ******************************************************************************/
+package net.sourceforge.jwebunit.javascript;
+
+public class JavascriptPrompt {
+	
+	private String message;
+	
+	private String input;
+	
+	public JavascriptPrompt(String message, String input) {
+		this.message=message;
+		this.input=input;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+}
